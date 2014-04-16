fb7992a121512d6857abb078c387fa52568d5446
Standa Vaštyl <stanislav@vastyl.cz>
2014-04-16 08:55:14 +0200

    correction comments

--- aprc.pl
+++ aprc.pl
@@ -1,10 +1,24 @@
 #!/usr/bin/perl -w
 
 #########################################################################
-### Name: AntiPhishing Rules Center for Spamassassin                    #
-### Script to detect phishing for spamassassin                          #
-### Phishing url addresses from web phishtank.com                       #
-### Created by stanislav.vastyl@vsb.cz                                  #
+### AntiPhishing Rules Center for Spamassassin                          
+### Script to detect phishing for spamassassin                          
+### Phishing url addresses from web phishtank.com                       
+###                                                                     
+### Copyright (C) 2013-2014 Petr Havlicek (ph@petrh.cz)
+###
+### This program is free software: you can redistribute it and/or modify
+### it under the terms of the GNU General Public License as published by
+### the Free Software Foundation, either version 3 of the License, or
+### any later version.
+###
+### This program is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+### GNU General Public License for more details.
+###
+### You should have received a copy of the GNU General Public License
+### along with this program. If not, see <http://www.gnu.org/licenses/>.
 #########################################################################
 
 use strict;
